07c3fd6c93bbde89e903546f128c6bba9e1503e0
Simon Hull <simonthomashull@gmail.com>
2024-11-21 09:48:11 -0700

    Quality of life and visual improvements to register form style

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,22 +11,39 @@ module.exports = {
         fontFamily: {
             'sans': ['poppins', 'sans-serif'],
         },
-        extend: {},
+
+        extend: {
+            colors: {
+                primary: {
+                    50: "#FFFCFC",
+                    100: "#FEF5F3",
+                    200: "#FDE8E4",
+                    300: "#FBD6CE",
+                    400: "#F9BFB3",
+                    500: "#F7A292",
+                    600: "#F4816A",
+                    700: "#F05B3D",
+                    800: "#97230B",
+                    900: "#491106",
+                    950: "#2F0B04"
+                },
+                secondary: {
+                    50: "#FCFCFD",
+                    100: "#F1F3F6",
+                    200: "#E0E4EA",
+                    300: "#C8CED9",
+                    400: "#A8B3C4",
+                    500: "#8292AA",
+                    600: "#5B6B86",
+                    700: "#384252",
+                    800: "#242B35",
+                    900: "#191D24",
+                    950: "#15181E"
+                }
+            },
+        },
     },
     plugins: [
-        require('daisyui'),
+        require('@tailwindcss/forms'),
     ],
-    daisyui: {
-        themes: [
-            {
-                listenup: {
-                    "primary": "#F05A3B",
-                    "secondary": "#384252",
-                    "accent": "#8292AA",
-                    "base-100": "#F6F0F0"
-                }
-            }
-        ],
-        styled: false,
-    }
 }
