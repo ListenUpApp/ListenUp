e08b929304afc1a8fcd2403c569cc13fcce25d26
Simon Hull <5488089+simonhull@users.noreply.github.com>
2026-06-09 12:13:49 -0600

    ✨ feat(shared): add isFirstInSeries series-starter helper

---
+++ sharedLogic/src/commonTest/kotlin/com/calypsan/listenup/client/core/SeriesSequenceTest.kt
@@ -0,0 +1,40 @@
+package com.calypsan.listenup.client.core
+
+import io.kotest.assertions.withClue
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+/**
+ * Mirrors the Go GetRandomBooks series-starter heuristic: standalones, blanks,
+ * prequels (0 / 0.5), and "1"-family sequences are starters; "2"+ are mid-series.
+ */
+class SeriesSequenceTest :
+    FunSpec({
+        test("isFirstInSeries matches the Go starter heuristic") {
+            val cases =
+                listOf(
+                    null to true,
+                    "" to true,
+                    "   " to true,
+                    "0" to true,
+                    "0.5" to true,
+                    "1" to true,
+                    "01" to true,
+                    "001" to true,
+                    "1.0" to true,
+                    "1.5" to true,
+                    "Book 1" to true,
+                    "Prequel" to true,
+                    "2" to false,
+                    "2.5" to false,
+                    "Book 2" to false,
+                    "10" to false,
+                    "11" to false,
+                )
+            for ((input, expected) in cases) {
+                withClue("isFirstInSeries(${input?.let { "\"$it\"" } ?: "null"}) should be $expected") {
+                    isFirstInSeries(input) shouldBe expected
+                }
+            }
+        }
+    })
