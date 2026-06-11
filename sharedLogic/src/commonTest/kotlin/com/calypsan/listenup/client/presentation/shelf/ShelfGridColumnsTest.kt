5d48be7b0f0e02430f6a0b25cd90babaf7c44db3
Simon Hull <5488089+simonhull@users.noreply.github.com>
2026-06-10 20:26:51 -0600

    🎨 style(sharedLogic): spotless-format ShelfGridColumnsTest

--- sharedLogic/src/commonTest/kotlin/com/calypsan/listenup/client/presentation/shelf/ShelfGridColumnsTest.kt
+++ sharedLogic/src/commonTest/kotlin/com/calypsan/listenup/client/presentation/shelf/ShelfGridColumnsTest.kt
@@ -3,14 +3,15 @@ package com.calypsan.listenup.client.presentation.shelf
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
-class ShelfGridColumnsTest : FunSpec({
-    test("compact width yields 2 columns") {
-        shelfGridColumns(ShelfGridWidth.Compact) shouldBe 2
-    }
-    test("medium width yields 4 columns") {
-        shelfGridColumns(ShelfGridWidth.Medium) shouldBe 4
-    }
-    test("expanded width yields 6 columns") {
-        shelfGridColumns(ShelfGridWidth.Expanded) shouldBe 6
-    }
-})
+class ShelfGridColumnsTest :
+    FunSpec({
+        test("compact width yields 2 columns") {
+            shelfGridColumns(ShelfGridWidth.Compact) shouldBe 2
+        }
+        test("medium width yields 4 columns") {
+            shelfGridColumns(ShelfGridWidth.Medium) shouldBe 4
+        }
+        test("expanded width yields 6 columns") {
+            shelfGridColumns(ShelfGridWidth.Expanded) shouldBe 6
+        }
+    })
