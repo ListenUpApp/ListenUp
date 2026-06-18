921a9fd16ead1ef4b29d260734a7a4957b866953
Simon Hull <5488089+simonhull@users.noreply.github.com>
2026-06-17 21:32:53 -0600

    🔥 chore(deps): drop unused kotlinx-collections-immutable

--- sharedUI/build.gradle.kts
+++ sharedUI/build.gradle.kts
@@ -139,7 +139,6 @@ kotlin {
             implementation(libs.kotlin.logging)
             implementation(libs.kotlinx.serialization.json)
             implementation(libs.kotlinx.datetime)
-            implementation(libs.kotlinx.collections.immutable)
 
             // Coil for image loading (multiplatform)
             implementation(libs.coil.compose)
