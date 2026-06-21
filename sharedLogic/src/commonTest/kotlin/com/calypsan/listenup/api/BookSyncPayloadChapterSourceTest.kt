1f0b4630bd60d465bac55e9ee0482632701de5e0
Simon Hull <5488089+simonhull@users.noreply.github.com>
2026-06-21 16:30:30 -0600

    ✨ feat(contract): add chapterSource provenance to BookSyncPayload

---
+++ sharedLogic/src/commonTest/kotlin/com/calypsan/listenup/api/BookSyncPayloadChapterSourceTest.kt
@@ -0,0 +1,12 @@
+package com.calypsan.listenup.api
+
+import com.calypsan.listenup.api.sync.ChapterSource
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class BookSyncPayloadChapterSourceTest : FunSpec({
+    test("ChapterSource has EMBEDDED, AUDNEXUS, USER in that order; default is EMBEDDED") {
+        ChapterSource.valueOf("EMBEDDED") shouldBe ChapterSource.EMBEDDED
+        ChapterSource.entries.map { it.name } shouldBe listOf("EMBEDDED", "AUDNEXUS", "USER")
+    }
+})
