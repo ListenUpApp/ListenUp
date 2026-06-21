5fff8db56dd9967d86112926477b5ab52684823b
Simon Hull <5488089+simonhull@users.noreply.github.com>
2026-06-21 16:38:13 -0600

    ✨ feat(contract): add ChapterInput DTO for setBookChapters

---
+++ sharedLogic/src/commonTest/kotlin/com/calypsan/listenup/api/ChapterInputTest.kt
@@ -0,0 +1,29 @@
+package com.calypsan.listenup.api
+
+import com.calypsan.listenup.api.dto.ChapterInput
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class ChapterInputTest : FunSpec({
+    test("valid input constructs") {
+        ChapterInput(id = "ch1", title = "Prologue", startTime = 0, duration = 1000).startTime shouldBe 0L
+    }
+    test("blank title is rejected") {
+        shouldThrow<IllegalArgumentException> { ChapterInput(id = "ch1", title = "  ", startTime = 0, duration = 1000) }
+    }
+    test("blank id is rejected") {
+        shouldThrow<IllegalArgumentException> { ChapterInput(id = " ", title = "A", startTime = 0, duration = 1000) }
+    }
+    test("negative startTime is rejected") {
+        shouldThrow<IllegalArgumentException> { ChapterInput(id = "ch1", title = "A", startTime = -1, duration = 1000) }
+    }
+    test("negative duration is rejected") {
+        shouldThrow<IllegalArgumentException> { ChapterInput(id = "ch1", title = "A", startTime = 0, duration = -1) }
+    }
+    test("title longer than MAX_TITLE is rejected") {
+        shouldThrow<IllegalArgumentException> {
+            ChapterInput(id = "ch1", title = "A".repeat(ChapterInput.MAX_TITLE + 1), startTime = 0, duration = 1000)
+        }
+    }
+})
