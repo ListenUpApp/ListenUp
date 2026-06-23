672802f567ed5ae9302e7c191b9749d1599b8de7
Simon Hull <5488089+simonhull@users.noreply.github.com>
2026-06-23 10:34:33 -0600

    🎨 chore(sharedUI): drop orphaned imports in BookEditScreen

--- sharedUI/src/commonMain/kotlin/com/calypsan/listenup/client/features/bookedit/BookEditScreen.kt
+++ sharedUI/src/commonMain/kotlin/com/calypsan/listenup/client/features/bookedit/BookEditScreen.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -254,7 +252,6 @@ private fun BookEditContent(
         } else {
             SingleColumnCardsLayout(state = state, onEvent = onEvent)
         }
-
     }
 }
 
